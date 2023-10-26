34c4169808f13fe591fa24ca03f046cf9f45cf01
Laxitasojitra <laxitasojitra00@gmail.com>
2023-10-26 13:14:43 +0530

    second commit

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "./../../components/Layout/Layout";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Make an API request to fetch the list of users
+    fetch("your_api_endpoint_here")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users: ", error));
+  }, []);
+
   return (
     <Layout title={"Dashboard - All Users"}>
       <div className="container-fluid m-3 p-3">
@@ -12,6 +22,12 @@ const Users = () => {
           </div>
           <div className="col-md-9">
             <h1>All Users</h1>
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>{user.name}</li>
+                // Assuming the user object has a "name" property
+              ))}
+            </ul>
           </div>
         </div>
       </div>
